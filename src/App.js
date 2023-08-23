6156476bc3ebd29a9d58629de34d714fda4c7a18
keithchasen <keithchasen89@gmail.com>
2023-08-23 23:42:26 +0200

    set up basic drag dropevents

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [dropzoneActive, setDropzoneActive] = useState(false);
+  const [files, setFiles] = useState([]);
+
+  const handleDrop = e => {
+    e.preventDefault();
+  }
+
   return (
     <div>
-      <div className='dropzone'>Drop your files here</div>
+      <div 
+        onDragOver={e => {
+          e.preventDefault();
+          setDropzoneActive(true);
+        }}
+        onDragLeave={e => {
+          e.preventDefault();
+          setDropzoneActive(false);
+        }}
+        onDrop={handleDrop}
+        className={`dropzone ${dropzoneActive ? ' active' : ''}`}
+        >
+          Drop your files here
+        </div>
     </div>
   );
 }
