49c6b5705f757a78feeed6ab7590b9bdd27abeb3
keithchasen <keithchasen89@gmail.com>
2023-08-24 14:23:20 +0200

    setting files, indexes and chunks before uploading

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   const [dropzoneActive, setDropzoneActive] = useState(false);
   const [files, setFiles] = useState([]);
+  const [currentFileIndex, setCurrentFileIndex] = useState(null);
+  const [lastUploadedFileIndex, setLastUploadedFileIndex] = useState(null)
+  const [currentChunkIndex, setCurrentChunkIndex] = useState(null); 
 
   const handleDrop = e => {
     e.preventDefault();
     setFiles([...files, e.dataTransfer.files])
   }
+
+  const readAndUploadCurrentChunk = () => {
+
+  }
+
+  useEffect(() => {
+    if (files.length > 0) {
+      if (currentFileIndex === null) {
+        setCurrentFileIndex(
+            lastUploadedFileIndex === null 
+            ? 0 
+            : lastUploadedFileIndex + 1
+          );
+      }
+    }
+    // when we drop new file 
+    // length of files changes
+    // so we're updating current file index
+  }, [files.length]);
+
+  useEffect(() => {
+    if (currentFileIndex !== null) {
+      setCurrentChunkIndex(0);
+    }
+    
+    // so when current file index changed
+    // we need to start uploading this file
+  }, [currentFileIndex]);
+
+  useEffect(() => {
+    if (currentChunkIndex !== null) {
+      readAndUploadCurrentChunk();
+    }
+    // if chunk index changed
+    // start uploading it
+  }, [currentChunkIndex]);
 
   return (
     <div>
