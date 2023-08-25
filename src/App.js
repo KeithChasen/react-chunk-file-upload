5ebcdcf6c50c321504696d36fedd586fac180d9d
keithchasen <keithchasen89@gmail.com>
2023-08-25 11:06:37 +0200

    added port to post request / fixed fetching file from files list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
+
+const chunkSize = 10 * 1024;
 
 function App() {
   const [dropzoneActive, setDropzoneActive] = useState(false);
@@ -10,11 +13,52 @@ function App() {
 
   const handleDrop = e => {
     e.preventDefault();
-    setFiles([...files, e.dataTransfer.files])
+    setFiles([...files, e.dataTransfer.files[0]])
   }
 
   const readAndUploadCurrentChunk = () => {
+    const reader = new FileReader();
+    const file = files[currentFileIndex];
 
+    if (!files) {
+      return;
+    }
+
+    // 0 => 0
+    // 1 => 1024
+    // 2 => 2048
+    // ...
+    const from = currentChunkIndex * chunkSize; 
+    
+    const to = from + chunkSize;
+
+    // binary part
+    const blob = file.slice(from, to);
+
+    reader.onload = e => uploadChunk(e);
+    reader.readAsDataURL(blob);
+  }
+
+  const uploadChunk = readerEvent => {
+    // need to know file which chunk we about to upload
+    const file = files[currentFileIndex];
+    const data = readerEvent.target.result;
+    const params = new URLSearchParams();
+
+    params.set('name', file.name);
+    params.set('size', file.size);
+    params.set('currentChunkIndex', currentChunkIndex);
+    params.set('totalChunks', Math.ceil(file.size / chunkSize));
+
+    const headers = {'Content-Type': 'application/octet-stream'};
+    const url = `http://localhost:4000/upload?${params.toString()}`;
+
+    axios.post(url, data, { headers })
+    .then(response => {
+
+      console.log(response, 'resp')
+      
+    })
   }
 
   useEffect(() => {
